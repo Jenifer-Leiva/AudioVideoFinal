9522acbfac05e800af877e925d4ea00934aa853e
Jenifer-Leiva <est.jenifer.leiva@unimilitar.edu.co>
2026-01-16 12:38:48 -0500

    magnetico 3 carro e imagenes

--- Magnetico/magnet.js
+++ Magnetico/magnet.js
@@ -12,3 +12,47 @@ document.querySelectorAll(".icono").forEach(icono => {
       popup.classList.remove("activo");
     });
   });
+
+
+  /* Imagenes de magnet 3 */
+
+  const imgBig4 = document.getElementById("imgBig4");
+  const imgBig5 = document.getElementById("imgBig5");
+  const btnDer  = document.getElementById("btnDer");
+  const btnIzq  = document.getElementById("btnIzq");
+
+  const estados = [
+    {
+      img4: "../Imagenes/magnetico/d2/philipsback.png",
+      img5: "../Imagenes/magnetico/d2/philiptxt.png"
+    },
+    {
+      img4: "../Imagenes/magnetico/d2/casetteback.png",
+      img5: "../Imagenes/magnetico/d2/cassettetxt.png"
+    },
+    {
+      img4: "../Imagenes/magnetico/d2/diskback.png",
+      img5: "../Imagenes/magnetico/d2/disktxt.png"
+    }
+  ];
+
+  let indice = 0;
+
+  btnDer.addEventListener("click", () => {
+    indice = (indice + 1) % estados.length;
+    cambiarImagenes();
+  });
+
+  btnIzq.addEventListener("click", () => {
+    indice = (indice - 1 + estados.length) % estados.length;
+    cambiarImagenes();
+  });
+
+  function cambiarImagenes() {
+    imgBig4.src = estados[indice].img4;
+    imgBig5.src = estados[indice].img5;
+
+    imgBig5.style.animation = "none";
+  imgBig5.offsetHeight; // fuerza reflow
+  imgBig5.style.animation = "fadeIn 5s forwards";
+  }
